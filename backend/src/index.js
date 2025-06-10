d5e221657a17f6bd5220ed842f07b32e5a6ec33c
Lawrence Ewa <ewalawrence1991@gmail.com>
2025-06-10 20:42:47 +0100

    build error fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,6 @@ import { app, server } from "./lib/socket.js";
 import path from "path";
 
 dotenv.config();
-
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
@@ -17,14 +16,15 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes);
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
